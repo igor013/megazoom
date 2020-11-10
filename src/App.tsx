4b43bec19ae8c0d970a84898566808bf70cfe33d
igor013 <igorhenriqueml@gmail.com>
2020-11-10 12:33:21 -0300

    Primeiros passos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p className = "contentimage p">Aguarde...</p>
+        <div className = "line"> </div>
+        <h5 className ="contentinfo">Contato para coberturas e eventos!</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
